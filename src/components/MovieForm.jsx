a072bd5d25c84aea31c3d17a10be95cefb3de6ad
jeffwang <wangjeffrey30@gmail.com>
2023-01-13 00:46:13 -0500

    rating system

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -2,21 +2,40 @@ import React from 'react'
 import Card from './shared/Card'
 import {useState} from 'react'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
 
 function MovieForm() {
     const [text, setText] = useState("")
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 8) {
+            setBtnDisabled(true)
+            setMessage('Reviews must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
   return (
     <Card> 
         <form>
             <h2>How did you find your latest movie?</h2>
+            <RatingSelect select = {(rating) => setRating(rating)}/>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder="Write a review"/>
-                <Button type="submit" version="primary">Log</Button>
+                <Button type="submit" version="primary"
+                 isDisabled = {btnDisabled}>Log</Button>
             </div>
+            {message && <div className='message'>
+                {message}
+                </div>}
         </form>
     </Card>
   )
