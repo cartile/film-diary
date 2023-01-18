ce0c144894da566af2405c08938194b1d46e01b7
jeffwang <wangjeffrey30@gmail.com>
2023-01-17 23:35:33 -0500

    motion changes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import MovieItem from './MovieItem'
 import PropTypes from 'prop-types'
+import {motion, AnimatePresence} from 'framer-motion'
 
 
 function MovieList({movie, handleDelete}) {
     if (!movie || movie.length === 0) {
         return <p>No movies logged yet.</p>
     }
+  return (<div className = 'feedback-list'>
+    <AnimatePresence>
+    {movie.map((item)=>(
+        <motion.div 
+          key={item.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit = {{opacity:0}}
+          >
+
+        <MovieItem
+        key = {item.id} 
+        item = {item}
+        handleDelete={handleDelete}/>
+        </motion.div>
+    ))}
+    </AnimatePresence>
+  </div>
+  )
+  
+      /*
   return (<div className = 'feedback-list'>
     {movie.map((item)=>(
         <MovieItem
@@ -15,7 +37,9 @@ function MovieList({movie, handleDelete}) {
         handleDelete={handleDelete}/>
     ))}
   </div>
-  )
+  ) 
+} 
+*/
 }
 
 MovieList.propTypes = {
