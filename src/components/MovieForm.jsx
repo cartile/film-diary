f09c7915dcb87d3b45de0a26037e7fbff0998006
jeffwang <wangjeffrey30@gmail.com>
2023-06-26 19:56:20 -0400

    movie form fix

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,65 +1,90 @@
-import React from 'react'
-import Card from './shared/Card'
-import {useState} from 'react'
-import Button from './shared/Button'
-import RatingSelect from './RatingSelect'
+import React, { useState } from 'react';
+import Card from './shared/Card';
+import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
+import { v4 as uuidv4 } from 'uuid';
 
-function MovieForm({handleAdd}) {
-    const [text, setText] = useState('')
-    const [rating, setRating] = useState(10)
-    const [btnDisabled, setBtnDisabled] = useState(true)
-    const [message, setMessage] = useState('')
+function MovieForm({ handleAdd }) {
+  const [movieName, setMovieName] = useState('');
+  const [reviewText, setReviewText] = useState('');
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-    const handleTextChange = (e) => {
-        if (text === '') {
-            setBtnDisabled(true)
-            setMessage(null)
-        } else if(text.trim().length <= 10) {
-            setBtnDisabled(true)
-            setMessage('Reviews must be at least 10 characters')
-        } else {
-            setMessage(null)
-            setBtnDisabled(false)
-        }
-        setText(e.target.value)
-    }
+  const handleMovieNameChange = (e) => {
+    const text = e.target.value;
+    setMovieName(text);
+    handleValidation(text, reviewText);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (text.trim().length > 10) {
-            const newMovie = {
-                text,
-                rating,
-            }
-            handleAdd(newMovie)
-        //handleAdd(newMovie)
-        //setBtnDisabled(true)
-        //setRating(10)
-            setText('')
+  const handleReviewTextChange = (e) => {
+    const text = e.target.value;
+    setReviewText(text);
+    handleValidation(movieName, text);
+  };
+
+  const handleValidation = (name, review) => {
+    if (name === '' || review === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (review.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Reviews must be at least 10 characters');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
-        
-        
+  };
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    if (reviewText.trim().length > 10) {
+      const newMovie = {
+        id: uuidv4(),
+        rating,
+        name: movieName, 
+        review: reviewText,
+      };
+      handleAdd(newMovie);
+      setMovieName('');
+      setReviewText('');
     }
+  };
+
   return (
-    <Card> 
-        <form onSubmit={handleSubmit}>
-            <h2>How did you find your latest movie?</h2>
-            <RatingSelect select = {(rating) => setRating(rating)}/>
-            <div className="input-group">
-                <input onChange={handleTextChange} type="text" placeholder="Movie Name"/>
-
-            </div>  
-            <div className="input-group">
-                <input onChange={handleTextChange} type="text" placeholder="Write a review"/>
-                <Button type="submit" version="primary"
-                 isDisabled = {btnDisabled}>Log</Button>
-            </div>
-            {message && <div className='message'>
-                {message}
-                </div>}
-        </form>
+    <Card>
+    <form onSubmit={handleSubmit}>
+    <h2>How did you find your latest movie?</h2>
+    <RatingSelect select={(rating) => setRating(rating)} />
+    <div className="input-group">
+        <label htmlFor="movieNameInput">Movie Name</label>
+        <input
+            id="movieNameInput"
+            onChange={handleMovieNameChange}
+            type="text"
+            placeholder="Enter the movie name"
+            value={movieName}
+        />
+    </div>
+    <div className="input-group">
+        <label htmlFor="reviewTextInput">Write a review</label>
+        <input
+            id="reviewTextInput"
+            onChange={handleReviewTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={reviewText}
+        />
+    <Button type="submit" version="primary" isDisabled={btnDisabled}>
+    Log
+    </Button>
+    </div>
+        {message && <div className='message'>{message}</div>}
+    </form>
     </Card>
-  )
+);
 }
 
-export default MovieForm
+export default MovieForm;
+
